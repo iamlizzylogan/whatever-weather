a5ef49cc440ca37f24ab190da42eaa092c184fac
Lizzy <bjozefowska98@gmail.com>
2019-07-25 17:04:18 +0200

    Create location dropdown template

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -9,6 +9,18 @@ class App extends Component {
         return (
             <div className="wrapper">
                 <Header title="Whatever Weather" />
+                <div className="widget">
+                    <section className="location">
+                        <div className="location__name">
+                            <span className="location__nameText">Londyn</span>
+                            <div className="location__nameOptions">
+                                <div className="location__nameOptions --option">Katowice</div>
+                                <div className="location__nameOptions --option">Londyn</div>
+                                <div className="location__nameOptions --option">Sosnowiec</div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </div>
         )
     }
