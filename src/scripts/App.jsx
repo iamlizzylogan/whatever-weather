90049b41eb4430dc40c09003cac6905d65594a52
Lizzy <bjozefowska98@gmail.com>
2019-07-27 10:45:23 +0200

    Add weather section

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -21,6 +21,34 @@ class App extends Component {
                         </div>
                     </section>
                     <section className="date">Tuesday, April 15th</section>
+                    <section className="weather">
+                        <span className="weather__type">Overcast</span>
+                        <div className="weather__data">
+                            <div className="weather__dataBasics">
+                                <img className="weather__dataBasics --image" src="assets/cloudy.png" alt="cloudy"/>
+                                <span className="weather__dataBasics --value">58</span>
+                                <span className="weather__dataBasics --unit">°F</span>
+                            </div>
+                            <div className="weather__dataDetails">
+                                <div>
+                                    <span className="weather__dataDetails --key">Precipitation: </span>
+                                    <span className="weather__dataDetails --value">100%</span>
+                                </div>
+                                <div>
+                                    <span className="weather__dataDetails --key">Humidity: </span>
+                                    <span className="weather__dataDetails --value">97%</span>
+                                </div>
+                                <div>
+                                    <span className="weather__dataDetails --key">Wind: </span>
+                                    <span className="weather__dataDetails --value">4 mph SW</span>
+                                </div>
+                                <div>
+                                    <span className="weather__dataDetails --key">Pollen Count: </span>
+                                    <span className="weather__dataDetails --value">36</span>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         )
