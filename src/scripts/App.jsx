00a6a515b3e9b8fe84b7b059f4401574af2b843f
Lizzy <bjozefowska98@gmail.com>
2019-07-28 11:04:56 +0200

    Create `Weather` component

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from "./Header";
 import Location from "./Location";
+import Weather from "./Weather";
 import getDateString from "./getDateString";
 
 class App extends Component {
@@ -37,34 +38,7 @@ class App extends Component {
                 <div className="widget">
                     <Location onChange={this.getWeatherInfo.bind(this)}/>
                     <section className="date">Tuesday, April 15th</section>
-                    <section className="weather">
-                        <span className="weather__type">Overcast</span>
-                        <div className="weather__data">
-                            <div className="weather__dataBasics">
-                                <img className="weather__dataBasics --image" src="assets/cloudy.png" alt="cloudy"/>
-                                <span className="weather__dataBasics --value">58</span>
-                                <span className="weather__dataBasics --unit">°F</span>
-                            </div>
-                            <div className="weather__dataDetails">
-                                <div>
-                                    <span className="weather__dataDetails --key">Precipitation: </span>
-                                    <span className="weather__dataDetails --value">100%</span>
-                                </div>
-                                <div>
-                                    <span className="weather__dataDetails --key">Humidity: </span>
-                                    <span className="weather__dataDetails --value">97%</span>
-                                </div>
-                                <div>
-                                    <span className="weather__dataDetails --key">Wind: </span>
-                                    <span className="weather__dataDetails --value">4 mph SW</span>
-                                </div>
-                                <div>
-                                    <span className="weather__dataDetails --key">Pollen Count: </span>
-                                    <span className="weather__dataDetails --value">36</span>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                    <Weather />
                     <section className="nutshell">
                         <ul>
                             <li className="nutshell__day --0">
